4cc92adff6d2db8bebd5e93daeb6b49b0371dbb2
nadimahmed1 <nadimarafat26@gmail.com>
2022-09-01 12:53:29 +0600

    update modify local storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,15 +1,27 @@
 // use local storage to manage cart data
 
 const addToDb = id => {
-    const quantity = localStorage.getItem(id);
-    if (quantity) {
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
+    let shoppingCart;
+
+    const getShoppingCart = localStorage.getItem('shopping-cart');
+    if (getShoppingCart) {
+        shoppingCart = JSON.parse(getShoppingCart);
     }
     else {
-
-        localStorage.setItem(id, 1)
+        shoppingCart = {};
     }
+
+    const quantity = shoppingCart[id];
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity
+        // localStorage.setItem(id, newQuantity);
+    }
+    else {
+        shoppingCart[id] = 1;
+        // localStorage.setItem(id, 1);
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 export { addToDb };
